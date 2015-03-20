c3b944130cbe1ae5abcf8b2690d3c04435f6f9e4
NatsuGirl <beatriz.sabater94@gmail.com>
2015-03-20 17:45:04 +0100

    Cambios carga mapa

--- Carga_Mapa/main.cpp
+++ Carga_Mapa/main.cpp
@@ -9,15 +9,118 @@ using namespace std;
 using namespace sf;
 int main()
 {
+    int _width;
+    int _height;
+    int _tileWidth;
+    int _tileHeight;
+    int _numLayers;
+ 
     XMLDocument doc;
     RenderWindow window(sf::VideoMode(480,320), "Fantasy Emblem");
-    doc.LoadFile("niveles/mapaTiled.tmx");
+    doc.LoadFile("niveles/mapa1.tmx");
     
-    XMLElement* elemento = doc.FirstChildElement("map");
+    //Tamanyo de mapa
+    XMLElement* map = doc.FirstChildElement("map");
+
+    map->QueryIntAttribute( "width", &_width );
+    map->QueryIntAttribute( "height", &_height );
+    map->QueryIntAttribute( "tilewidth", &_tileWidth );
+    map->QueryIntAttribute( "tileheight", &_tileHeight );
     
-    int ancho = elemento->IntAttribute("width");
-    int alto = elemento->IntAttribute("height");
+    //Imagen del tileset
+    XMLElement *img = map->FirstChildElement("tileset")->FirstChildElement("image");
+    const char *filename = img->Attribute("source");
+    /*
+    //Cuantas capas tiene mi XML
+    XMLElement *layer = map->FirstChildElement("layer");
+    while(layer){
+        _numLayers++;
+        layer = layer->NextSiblingElement("layer");
+    }
     
+    //Cargando GID multiples capas  
+    XMLElement *data[_numLayers];
+    data[0] = map->FirstChildElement("layer")->FirstChildElement("data")->FirstChildElement("tile");    
+    
+    int ***_tilemap;
+    
+    _tilemap = new int**[_numLayers];
+    for(int i=0; i<_numLayers; i++){
+        _tilemap[i] = new int*[_height];
+    }
+    for(int l=0; l<_numLayers; l++){
+        for(int y=0; y<_height; y++){
+            _tilemap[l][y]=new int[_width];
+        }
+    }
+    */
+    /*
+    sf::Sprite ****_tilemapSprite;
+    _tilemapSprite = new sf::Sprite***[_numLayers];
+    for(int l=0; l<_numLayers; l++){
+        _tilemapSprite[l] = new sf::Sprite**[_height];
+    }
+    */
+    /*
+    for(int l=0; l<_numLayers; l++){
+        for(int y=0; y<_height;y++){
+            _tilemapSprite[l][y] = new sf::Sprite*[_width];
+            for(int x=0; x<_width; x++){
+                //...
+                _tilemapSprite[l][y][x] = new sf::Sprite();
+            }
+        }
+    }
+    */
+    /*
+    for(int l=0; l<_numLayers;l++){
+        for(int y=0; y<_height;y++){
+            for(int x=0; x<_width; x++){
+                data[l]->QueryIntAttribute("gid", &_tilemap[l][y][x]);
+                //Avanzo al siguiente tag
+                data[l]=data[l]->NextSiblingElement("tile");
+                
+            }
+        }
+    }
+    */
+    /*
+    /////////////////////////////////////////////////////////////////////////
+    Texture textura;
+    const IntRect FU(16,16,16,16);
+    
+    if (!textura.loadFromFile("resources/patron.png"))
+    {
+        std::cerr << "Error cargando la imagen sprites.png";
+        exit(0);
+    }
+    /////////////////////////////////////////////////////////////////////////
+    
+    
+    for(int l=0; l<_numLayers;l++){
+        for(int y=0; y<_height;y++){
+            for(int x=0; x<_width; x++){
+                
+                int gid = _tilemap[l][y][x]-1;
+                
+                //if(gid>=_tsw*_tsh){
+                //    std::cerr << "Error, git at (l,x,y)=("<<endl; 
+                //
+                }else if(gid>0){
+                    //Si fuera 0 no creo sprite...
+                    //_tilemapSprite[l][y][x] = new sf::Sprite(_tilesetTexture,_tilesetSprite[gid]->getTextureRect());
+                    _tilemapSprite[l][y][x] = new sf::Sprite(textura,FU);
+                    _tilemapSprite[l][y][x]->setPosition(x*_tileWidth,y*_tileHeight);
+                }else{
+                    _tilemapSprite[l][y][x] = NULL;
+                }
+            }
+        }
+    }
+    */
+
+    
+    /*
     Sprite sprite;
     Texture textura;
     
@@ -99,6 +202,25 @@ int main()
     
     
 
+    return 0;
+     */
+    
+    
+    while(window.isOpen())
+    {
+        Event evento;
+        
+        while(window.pollEvent(evento))
+        {
+            switch(evento.type)
+            {
+                case Event::Closed:
+                    window.close();
+                break;
+                
+            }
+        }
+    }
     return 0;
 }
 
