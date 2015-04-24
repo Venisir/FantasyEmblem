91415be0983bc043541de5fe63ac36937f15d53a
oli92 <oli92_@hotmail.com>
2015-04-24 17:20:54 +0200

    armas y objetos

--- FantasyEmblem/clases/Armas.cpp
+++ FantasyEmblem/clases/Armas.cpp
@@ -6,13 +6,53 @@
  */
 
 #include "../headers/Armas.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <stdio.h>
+#include <string.h>
 
-Armas::Armas() {
+using namespace std;
+using namespace sf;
+
+
+Armas::Armas(string nom, string tip, int pes, int pod, int golp, int alcan, int usos ) {
+    if(nom!=NULL && tip!=NULL){
+        Nombre=nom;
+        Tipo=tip;
+    }
+    Peso=pes;
+    Poder=pod;
+    Golpe=golp;
+    Alcance=alcan;
+    Usos=usos;
+    
 }
 
-Armas::Armas(const Armas& orig) {
-}
 
 Armas::~Armas() {
 }
 
+Armas::getPeso(){
+    return Peso;
+}
+
+Armas::getPoder(){
+    return Poder;
+}
+
+Armas::getGolpe(){
+    return Golpe;
+}
+
+Armas::getAlcance(){
+    return Alcance;    
+}
+
+Armas::getUsos(){
+    return Usos;
+}
+
+Armas::setUsos(int uso){
+    Usos=Usos-uso;
+}
