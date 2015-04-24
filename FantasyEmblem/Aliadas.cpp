ba119783db6557625ee0b671e9bce257da694af8
constanzaccg <constanzacasquetgonzalez@hotmail.com>
2015-04-24 18:19:10 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	FantasyEmblem/clases/Aliadas.cpp
    	FantasyEmblem/clases/Cofre.cpp
    	FantasyEmblem/headers/Cofre.h
    
    Modificaciones cofre y aliadas

---
+++ FantasyEmblem/Aliadas.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Aliadas.cpp
+ * Author: Constanza
+ * 
+ * Created on 15 de abril de 2015, 9:40
+ */
+
+#include "../headers/Aliadas.h"
+
+Aliadas::Aliadas() {
+}
+
+Aliadas::Aliadas(const Aliadas& orig) {
+}
+
+Aliadas::~Aliadas() {
+}
+
